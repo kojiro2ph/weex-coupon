4e13bc56bdeba8e9769f5d54dc0f75eb524ab33f
Kojiro Hamada <kojiro2ph@gmail.com>
2026-05-19 14:40:08 +0900

    change return data

--- main.py
+++ main.py
@@ -12,13 +12,7 @@ app = FastAPI(
 @app.get("/api/data")
 def get_sample_data():
     return {
-        "data": [
-            {"id": 1, "name": "Sample Item 1", "value": 100},
-            {"id": 2, "name": "Sample Item 2", "value": 200},
-            {"id": 3, "name": "Sample Item 3", "value": 300}
-        ],
-        "total": 3,
-        "timestamp": "2024-01-01T00:00:00Z"
+        "myname": "kojiro hamada"
     }
 
 
